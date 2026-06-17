96d19e35a5a1cc9ee0754fcb8611146f12dadf48
Samuel <samuel.pzin2006@gmail.com>
2026-06-17 10:15:31 -0300

    Alicerce do projeto

---
+++ script.js
@@ -0,0 +1,144 @@
+let alunos =
+JSON.parse(localStorage.getItem("alunos")) || [];
+
+function mostrarTela(id){
+
+    document
+    .querySelectorAll("section")
+    .forEach(sec =>
+        sec.classList.add("oculto")
+    );
+
+    document
+    .getElementById(id)
+    .classList.remove("oculto");
+}
+
+function salvarAluno(){
+
+    const nome =
+    document.getElementById("nome").value;
+
+    const matricula =
+    document.getElementById("matricula").value;
+
+    if(nome === "" || matricula === ""){
+        alert("Preencha os campos obrigatórios");
+        return;
+    }
+
+    const existe =
+    alunos.find(
+        aluno => aluno.nome === nome
+    );
+
+    if(existe){
+        alert("Aluno já cadastrado");
+        return;
+    }
+
+    alunos.push({
+
+        nome,
+
+        matricula,
+
+        idade:
+        document.getElementById("idade").value,
+
+        curso:
+        document.getElementById("curso").value,
+
+        escola:
+        document.getElementById("escola").value
+
+    });
+
+    localStorage.setItem(
+        "alunos",
+        JSON.stringify(alunos)
+    );
+
+    listarAlunos();
+
+    mostrarTela("lista");
+}
+
+function listarAlunos(){
+
+    const busca =
+    document
+    .getElementById("busca")
+    ?.value
+    .toLowerCase() || "";
+
+    const tabela =
+    document.getElementById("tabela");
+
+    tabela.innerHTML = "";
+
+    alunos
+    .filter(a =>
+        a.nome.toLowerCase()
+        .includes(busca)
+    )
+    .forEach((aluno,index)=>{
+
+        tabela.innerHTML += `
+        <tr>
+            <td>${aluno.nome}</td>
+            <td>${aluno.matricula}</td>
+            <td>${aluno.curso}</td>
+
+            <td>
+
+                <button
+                onclick="editarAluno(${index})">
+                ✏️
+                </button>
+
+                <button
+                onclick="excluirAluno(${index})">
+                🗑️
+                </button>
+
+            </td>
+        </tr>
+        `;
+    });
+}
+
+function excluirAluno(index){
+
+    alunos.splice(index,1);
+
+    localStorage.setItem(
+        "alunos",
+        JSON.stringify(alunos)
+    );
+
+    listarAlunos();
+}
+
+function editarAluno(index){
+
+    const novoNome =
+    prompt(
+        "Novo nome:",
+        alunos[index].nome
+    );
+
+    if(novoNome){
+
+        alunos[index].nome = novoNome;
+
+        localStorage.setItem(
+            "alunos",
+            JSON.stringify(alunos)
+        );
+
+        listarAlunos();
+    }
+}
+
+listarAlunos();
